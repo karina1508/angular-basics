5fb85a5629b01aafe06d37c25d8fed0e63e824a7
karyna <karynazhadan@gmail.com>
2021-01-02 23:13:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Dynamic title';
+  number = 42;
+  arr = [1, 2, 3];
+
+  obj = {a: 1, b: {c: 2}};
+  image = 'https://e7.pngegg.com/pngimages/599/417/png-clipart-react-angularjs-javascript-library-node-js-github-symmetry-web-application.png'
+
+  constructor() {
+    setTimeout(() => {
+      this.image = 'https://e7.pngegg.com/pngimages/269/178/png-clipart-angularjs-computer-icons-javascript-graphics-javascript-icon-trademark-logo-thumbnail.png'
+    }, 5000);
+  }
+
 }
