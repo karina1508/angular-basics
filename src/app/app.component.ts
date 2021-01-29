36eb649b213c1349602cced27adfa749c1902855
karyna <karynazhadan@gmail.com>
2021-01-29 21:08:05 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,32 @@ export class AppComponent {
   arr = [1, 2, 3];
 
   obj = {a: 1, b: {c: 2}};
-  image = 'https://e7.pngegg.com/pngimages/599/417/png-clipart-react-angularjs-javascript-library-node-js-github-symmetry-web-application.png'
+  image = 'https://e7.pngegg.com/pngimages/599/417/png-clipart-react-angularjs-javascript-library-node-js-github-symmetry-web-application.png';
+  inputValue = '';
+  @Input() data: string;
 
   constructor() {
-    setTimeout(() => {
-      this.image = 'https://e7.pngegg.com/pngimages/269/178/png-clipart-angularjs-computer-icons-javascript-graphics-javascript-icon-trademark-logo-thumbnail.png'
-    }, 5000);
+    /*   setTimeout(() => {
+         this.image = 'https://e7.pngegg.com/pngimages/269/178/png-clipart-angularjs-computer-icons-javascript-graphics-javascript-icon-trademark-logo-thumbnail.png'
+       }, 5000);*/
+
+  }
+
+  /*  onInput(event?): void {
+      console.log(event);
+      this.inputValue = event.target.value;
+    }*/
+  onInput(event?: KeyboardEvent): void {
+    console.log(event);
+    this.inputValue = (<HTMLInputElement> event.target).value;
+  }
+
+  onBlur(str: string): void {
+    this.inputValue = str;
+  }
+
+  onClick(): void {
+    console.log('Was click');
   }
 
 }
